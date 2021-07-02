a37b46c020f881c732e693647ecce225fd49a6ce
Stephan <sgriesel@gmail.com>
2021-07-02 06:23:20 +0200

    db setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,21 @@
 const path = require('path')
 
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  if (stage === "develop-html" || stage === "build-html") {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["main"],
+      },
+    })
+  } else {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["browser", "module", "main"],
+      },
+    })
+  }
+}
+
 exports.createPages = ({graphql,actions}) => {
     const {createPage} = actions;
     const truckTemplate = path.resolve('src/templates/truckTemplate.js')
