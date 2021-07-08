e7f6e427235e9680cbbd39d48cc7a8375ba91a69
Stephan <sgriesel@gmail.com>
2021-07-08 06:20:41 +0200

    set state registration form

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, Input, Button} from '../components/common'
 
 const Register = () => {
+  const [formValues, setFormValues] = useState({
+    email:'',
+    password:'',
+    confirmPassword:''
+  })
+
+  function handeInputChange(e){
+    e.persist();
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [e.target.name]: e.target.value
+    }))
+  }
   function handleSubmit (e){
     e.preventDefault();
+    console.log("Form entries", formValues)
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Input type="email" placeholder="email" required />
-      <Input type="password" placeholder="password" required minLength={7}/>
-      <Input type="password" placeholder="confirm password" required minLength={7}/>
+      <Input onChange={handeInputChange} value={formValues.email} type="email" placeholder="email" required name="email"/>
+      <Input onChange={handeInputChange} value={formValues.password} type="password" placeholder="password" required minLength={7} name="password"/>
+      <Input onChange={handeInputChange} value={formValues.confirmPassword} type="password" placeholder="confirm password" required minLength={7} name="confirmPassword"/>
       <Button type="submit" block>
         Register
       </Button>
