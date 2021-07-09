bcb6c601750d634933af9c51d5fc6bb5ac49ce20
Stephan <sgriesel@gmail.com>
2021-07-09 08:40:12 +0200

    add validation login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,14 +7,19 @@ import {Form, Input, Button} from '../components/common'
 const Login = () => {
     const [formValues, setFormValues] = useState({email: '', password: ''});
     const {firebase} = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('')
 
     function handleSubmit(e){
         e.preventDefault();
-        firebase.login({email:formValues.email, password: formValues.password })
+        firebase.login({email:formValues.email, password: formValues.password }).catch(error => {
+            console.log("Login Validation Error: ", error)
+            setErrorMessage(error.message);
+        })
     }
     
     function handleInputChange(e){
         e.persist();
+        setErrorMessage('');
         setFormValues(currentValues => ({
             ...currentValues,
             [e.target.name]: e.target.value
@@ -24,8 +29,13 @@ const Login = () => {
     return(
     <section>
         <Form onSubmit={handleSubmit}>
-            <Input value={formValues.email} name="email" onChange={handleInputChange} placeholder="email" type="email"></Input>
-            <Input value={formValues.password} name="password" onChange={handleInputChange} placeholder="password" type="password"></Input>
+            <Input required value={formValues.email} name="email" onChange={handleInputChange} placeholder="email" type="email"></Input>
+            <Input required value={formValues.password} name="password" onChange={handleInputChange} placeholder="password" type="password"></Input>
+            {!!errorMessage &&
+            <span>
+                {errorMessage}
+            </span>
+            }
             <Button type="submit" block>Login</Button>
         </Form>
     </section>
