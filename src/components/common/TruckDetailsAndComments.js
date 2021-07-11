225474eb2b916490dd7d96423ac2e0f32926a543
Stephan <sgriesel@gmail.com>
2021-07-11 14:01:29 +0200

    render comments

--- src/components/common/TruckDetailsAndComments.js
+++ src/components/common/TruckDetailsAndComments.js
@@ -1,12 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const TruckDetailsAndComments = ({firebase, truckId}) => {
+
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const unsubscribe = firebase.subscribeToTruckDetailsAndComments({
       truckId,
       onSnapshot: (snapshot) => {
         console.log("Snapshot data: ", snapshot)
+        const snapshotComments = [];
+        snapshot.forEach(doc => {
+          snapshotComments.push({
+            id:doc.id,
+            ...doc.data()
+          })
+        })
+        setComments(snapshotComments);
       }
     })
     return () => {
@@ -15,9 +25,21 @@ export const TruckDetailsAndComments = ({firebase, truckId}) => {
       }
     }
   },[])
+
+  console.log("Comment data", comments);
   return(
     <div>
-      Comment Test
+      {comments.map(comment => (
+        <div>
+          <div key={comment.id}>
+            {comment.username}
+          </div>
+          <div>
+            {comment.text}
+          </div>
+        </div>      
+      ))}
+      
     </div>
   )
 }
