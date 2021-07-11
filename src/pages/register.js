20a10cd2d06301113eaebfbe722f05b147d5fa84
Stephan <sgriesel@gmail.com>
2021-07-11 09:00:47 +0200

    register validation

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,9 +1,10 @@
 import React, {useState, useContext} from 'react'
-import {Form, Input, Button} from '../components/common'
+import {Form, Input, Button, ErrorMessage} from '../components/common'
 import {FirebaseContext} from '../components/Firebase'
 
 const Register = () => {
   const {firebase} = useContext(FirebaseContext);
+  const [errorMessage, setErrorMessage] = useState('');
   const [formValues, setFormValues] = useState({
     email:'',
     password:'',
@@ -12,6 +13,7 @@ const Register = () => {
 
   function handeInputChange(e){
     e.persist();
+    setErrorMessage('');
     setFormValues(currentValues => ({
       ...currentValues,
       [e.target.name]: e.target.value
@@ -24,7 +26,11 @@ const Register = () => {
       firebase.register({
         email: formValues.email,
         password: formValues.password
+      }).catch(error => {
+        setErrorMessage(error.message);
       })
+    } else {
+      setErrorMessage('Password fields not matching, try again!')
     }
     console.log("Form entries", formValues)
   }
@@ -34,6 +40,11 @@ const Register = () => {
       <Input onChange={handeInputChange} value={formValues.email} type="email" placeholder="email" required name="email"/>
       <Input onChange={handeInputChange} value={formValues.password} type="password" placeholder="password" required minLength={7} name="password"/>
       <Input onChange={handeInputChange} value={formValues.confirmPassword} type="password" placeholder="confirm password" required minLength={7} name="confirmPassword"/>
+      {!!errorMessage && 
+        <ErrorMessage>
+          {errorMessage}
+        </ErrorMessage>
+      }
       <Button type="submit" block>
         Register
       </Button>
